678da09453117553496bd36cd45d9af8d1a3d347
Dody Gunawinata <dodyg@silverkeytech.com>
2017-11-18 15:20:46 +0200

    WIP - fix build error

--- Vlatava.Core/src/Protocols/ComplexSyndicationItem.cs
+++ Vlatava.Core/src/Protocols/ComplexSyndicationItem.cs
@@ -2,14 +2,17 @@ using Microsoft.SyndicationFeed;
 using Microsoft.SyndicationFeed.Rss;
 using System.Linq;
 using System;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Vlatava.Core.Protocols
 {
-    public class ComplexSyndicationItem 
+    public class ComplexSyndicationItem
     {
-        public ISyndicationItem Item {get; set;}
+        public ISyndicationItem Item { get; set; }
 
-        public Outline Outline {get;set;}
+        public Outline Outline { get; set; }
 
         public ComplexSyndicationItem(ISyndicationItem item, ISyndicationContent outline = null)
         {
@@ -21,6 +24,40 @@ namespace Vlatava.Core.Protocols
                     Attributes = outline.Attributes.ToDictionary(x => x.Name, x => x.Value)
                 };
             }
+        }
+    }
+
+    public static class SyndicationReader
+    {
+        public static async Task<ComplexSyndicationItem> Get(Uri url)
+        {
+            var parser = new RssParser();
+            var items = new List<ComplexSyndicationItem>();
+
+            using (var xmlReader = XmlReader.Create(url.ToString(), new XmlReaderSettings { Async = true }))
+            {
+                var feedReader = new RssFeedReader(xmlReader);
+
+                while (await feedReader.Read())
+                {
+                    switch (feedReader.ElementType)
+                    {
+                        case SyndicationElementType.Item:
+                            //ISyndicationContent is a raw representation of the feed
+                            ISyndicationContent content = await feedReader.ReadContent();
+
+                            ISyndicationItem item = parser.CreateItem(content);
+                            ISyndicationContent outline = content.Fields.FirstOrDefault(f => f.Name == "source:outline");
+
+                            items.Add(new ComplexSyndicationItem(item, outline));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return items;
         }
     }
 }
---
+++ Vlatava.Web/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Vlatava.Core.Protocols;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Vlatava.Web
+{
+
+ public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are the four default services available at Configure
+            app.Run(async context =>
+            {
+                var items = await SyndicationReader.Get(new Uri("http://scripting.com/rss.xml"));
+                var str = new StringBuilder();
+                str.Append("<ul>");
+                foreach (var i in items)
+                {
+                    str.Append($"<li>{i.Item.Description} - <span style=\"color:red;\">");
+                    if (i.Outline != null)
+                    {
+                        str.Append("<ul>");
+                        foreach (var o in i.Outline.Attributes)
+                        {
+                            str.Append($"<li>{o.Key} - {o.Value}</li>");
+                        }
+                        str.Append("</ul>");
+                    }
+                    str.Append("</li>");
+                }
+                str.Append("</ul>");
+
+                context.Response.Headers.Add("Content-Type", "text/html");
+                await context.Response.WriteAsync($@"
+                <html>
+                    <head>
+                        <link rel=""stylesheet"" type=""text/css"" href=""http://fonts.googleapis.com/css?family=Germania+One"">
+                        <style>
+                         body {{
+                            font-family: 'Germania One', serif;
+                            font-size: 24px;
+                        }}
+                        </style>
+                    </head>
+                    <body>
+                        {str.ToString()}
+                    </body>
+                </html>
+                ");
+            });
+        }
+    }
+
+}
