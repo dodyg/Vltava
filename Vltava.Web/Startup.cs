113978c59b1e8c52e9096b045053da5b4515e44e
Dody Gunawinata <dodyg@silverkeytech.com>
2017-11-26 18:26:47 +0200

    WIP implementing TPL

--- Vltava.Web/Startup.cs
+++ Vltava.Web/Startup.cs
@@ -14,12 +14,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Optional;
 using Optional.Unsafe;
 using Vltava.Core.Features;
+using System.Threading.Tasks.Dataflow;
 
 namespace Vltava.Web
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection  services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<SystemFolders>();
         }
@@ -28,41 +29,84 @@ namespace Vltava.Web
         {
             var sysFolders = app.ApplicationServices.GetService<SystemFolders>();
 
+            var opmlReading = new TransformBlock<string, string>(async fileName =>
+                              {
+                                  var subscriptionListFile = sysFolders.SubscriptionsFile(fileName);
+                                  if (!subscriptionListFile.HasValue)
+                                      throw new ArgumentException($"{subscriptionListFile} does not exist");
+
+                                  var subscription = await File.ReadAllTextAsync(subscriptionListFile.ValueOrFailure());
+
+                                  return subscription;
+                              });
+
+            var opmlParsing = new TransformBlock<string, Opml>(subscription =>
+            {
+                var opml = Opml.Parse(subscription);
+
+                if (opml.IsFalse)
+                    throw opml.ExceptionObject;
+
+                return opml.Value;
+            });
+
+            var syndicationSourceUrls = new TransformBlock<Opml, List<Uri>>(opml =>
+            {
+                var outlines = opml.Find("type", "rss");
+                return outlines.Where(o => o.Attributes.ContainsKey("url")).Select(o => new Uri(o["url"])).ToList();
+            });
+
+            var syndications = new TransformBlock<List<Uri>, List<ComplexSyndication>>(async uris =>
+            {
+                var items = await SyndicationReader.Get(uris.ToArray());
+                return items;
+            });
+
+            SyndicationReader.SyndicationItemStream.Subscribe(x => Console.WriteLine(x.Item.Title));
+
+            var template = new TransformBlock<string, string>(async filename =>
+            {
+                var templateFile = sysFolders.TemplateFile(filename);
+                if (!templateFile.HasValue)
+                    throw new ArgumentException($"{templateFile} does not exist");
+
+                var tmplt = await File.ReadAllTextAsync(templateFile.ValueOrFailure());
+                return tmplt;
+            });
+
+            var output = new TransformBlock<Tuple<string, List<ComplexSyndication>>, string>(input =>
+           {
+               System.Console.WriteLine("Rendering");
+               var r = new HtmlRender();
+               return r.Render(input.Item1, input.Item2);
+           });
+
+
             //These are the four default services available at Configure
             app.Run(async context =>
             {
                 try
                 {
-                    var subscriptionListFile =  sysFolders.SubscriptionsFile("tech.opml");
-                    if (!subscriptionListFile.HasValue)
-                        throw new ArgumentException($"{subscriptionListFile} does not exist");
-
-                    var subscription = await File.ReadAllTextAsync(subscriptionListFile.ValueOrFailure());
-                    var opml = Opml.Parse(subscription);
-
-                    if (opml.IsFalse)
+                    var render = new ActionBlock<string>(async input =>
                     {
-                        await context.Response.WriteAsync("Error in loading");
-                        return;
-                    }
+                        context.Response.Headers.Add("Content-Type", "text/html");
+                        await context.Response.WriteAsync(input);
+                    });
 
-                    var outlines = opml.Value.Find("type", "rss");
-                    var urls = outlines.Where(o => o.Attributes.ContainsKey("url")).Select(o => new Uri(o["url"]));
+                    opmlReading.LinkTo(opmlParsing);
+                    opmlParsing.LinkTo(syndicationSourceUrls);
+                    syndicationSourceUrls.LinkTo(syndications);
 
-                    var items = await SyndicationReader.Get(urls.ToArray());
-                    SyndicationReader.SyndicationItemStream.Subscribe(x => Console.WriteLine(x.Item.Title));
+                    var join = new JoinBlock<string, List<ComplexSyndication>>();
+                    template.LinkTo(join.Target1);
+                    syndications.LinkTo(join.Target2);
+                    join.LinkTo(output);
+                    output.LinkTo(render);
 
-                    var templateFile = sysFolders.TemplateFile("default.scriban-html");
-                    if (!templateFile.HasValue)
-                        throw new ArgumentException($"{templateFile} does not exist");
+                    template.Post("default.scriban-html");
+                    opmlReading.Post("tech.opml");
 
-                    var template = await File.ReadAllTextAsync(templateFile.ValueOrFailure());
-
-                    var render = new HtmlRender();
-                    var output = render.Render(template, items);
-
-                    context.Response.Headers.Add("Content-Type", "text/html");
-                    await context.Response.WriteAsync(output);
+                    await render.Completion;
                 }
                 catch (Exception ex)
                 {
